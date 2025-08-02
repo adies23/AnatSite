f42be01ac28de6ab67e97c8220aba8bb30c7fb5e
Adi Eshete <adieshete23@gmail.com>
2025-08-02 17:39:02 +0300

    add view changes

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-JLTIDQHR.mjs";import"./chunk-WZM2KTAJ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-3QQZ7W3H.mjs";import"./chunk-WZM2KTAJ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
